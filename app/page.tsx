0415cf7cd783986f794b7f8241100c496b32d765
vuongquocdat0211 <66472082+vuongquocdat0211@users.noreply.github.com>
2026-05-28 18:18:40 +0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div style={{
+      padding: 40,
+      fontFamily: 'Arial'
+    }}>
+      <h1>🍓 WEBSITE ĐẶT DÂU TÂY</h1>
+
+      <p>Website đang hoạt động thành công.</p>
+    </div>
+  )
+}
